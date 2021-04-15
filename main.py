76b877564e117b04962a75b124a991e90a72b52e
Necrocaustic <rbrownr2d2@yahoo.com>
2021-04-14 22:48:08 -0400

    Changes by Zach.Tech

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import discord
 from discord.ext import commands
 
-
 import sys
-from config import token
+from config import *
 import traceback
+
 """
 intents = discord.Intents.default()
 intents.typing = False
 intents.members = True
 """
+
+
 # UPDATE CHANNEL IDS SOMEWHERE IF YOU RUN OUT OF TIME IN CLASS
 def get_prefix(bot, message):
+    prefixes = ['~']
+    if not message.guild:
+        return '?'
+    return commands.when_mentioned_or(*prefixes)(bot, message)
 
-bot = commands.Bot(command_prefix="~",
-                   activity=discord.Activity(type=discord.ActivityType.playing,
-                                             name='with Phant0m'),
-                   intents=discord.Intents.all())
 
 initial_extensions = ['cogs.admin',
                       'cogs.owner',
@@ -24,10 +26,20 @@ initial_extensions = ['cogs.admin',
                       'cogs.easter'
                       ]
 
+bot = commands.Bot(command_prefix=get_prefix,
+                   activity=discord.Activity(type=discord.ActivityType.playing,
+                                             name='with Phant0m'),
+                   intents=discord.Intents.all())
+
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        bot.load_extension(extension)
+
+
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
-    print('Build:', build)
+    print('Version: ', version)
 
 
 @bot.command(aliases=['objectiveadd'])
@@ -213,5 +225,5 @@ async def on_command_error(ctx, error):
             'There was an error. Try running the command again with the right parameters, or use ~reportissue to let my developer know about you seeing this. If you are running a command that require arguements, check to ensure all of the fields are filled. And yes, ~fetchmessage REQUIRES a message ID. You won\'t get anything without it.')
 
 
-#keep_alive() don't need this
+# keep_alive() don't need this
 bot.run(token, bot=True, reconnect=True)
